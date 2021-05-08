34e0dbe9f48749c60f06309b8c879d96c6f59182
Saravana <saravana.g@tranxitgroup.com>
2021-05-08 18:44:16 +0530

    first commit

---
+++ TBV+Text/textViewController.swift
@@ -0,0 +1,29 @@
+//
+//  textViewController.swift
+//  TBV+Text
+//
+//  Created by Saravana on 08/05/21.
+//
+
+import UIKit
+
+class textViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
