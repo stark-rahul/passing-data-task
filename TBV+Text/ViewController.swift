67bd2cc50b67dd5f3922f475b773e571b7f54af4
Saravana <saravana.g@tranxitgroup.com>
2021-05-08 19:03:20 +0530

    add all files

--- TBV+Text/ViewController.swift
+++ TBV+Text/ViewController.swift
@@ -7,13 +7,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct Data {
+    var fullname: String
+}
 
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    var saveDatas = [Data]()
+    
+    
+    @IBOutlet var table: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        table.delegate = self
+        table.dataSource = self
+        title = "savedata"
+//        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(handleAddData))
+        
     }
+    //mark: - Selectors
+    @objc func handleAddData(){
+        
+        let controller = textViewController()
+        controller.delegate = self
+        
+        self.present(UINavigationController(rootViewController: controller), animated: true, completion: nil)
+    }
+    
+   
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return saveDatas.count
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = saveDatas[indexPath.row].fullname
+        return cell
+    }
+}
 
-
+extension ViewController: addDataDelegate {
+    
+    func addData(saveDatas: Data) {
+        self.dismiss(animated: true) {
+            self.saveDatas.append(saveDatas)
+            self.table.reloadData()
+        }
+    }
 }
 
--- TBV+Text/textViewController.swift
+++ TBV+Text/textViewController.swift
@@ -7,23 +7,62 @@
 
 import UIKit
 
+protocol addDataDelegate {
+    func addData(saveDatas: Data)
+}
+
+
 class textViewController: UIViewController {
+    
+    var delegate: addDataDelegate?
+    
+    
+    @IBOutlet var textField: UITextField!
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Add the Data here"
+        view.backgroundColor = .systemPink
+        
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(handleDone))
+        
+        view.addSubview(textField)
+        textField.placeholder = "fullname"
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        
+        textField.becomeFirstResponder()
+       //self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(handleCancel))
 
-        // Do any additional setup after loading the view.
+//
+
+        
     }
     
 
-    /*
+   
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func handleDone() {
+        
+//        print("handle done..")
+        guard let textname = textField.text, textField.hasText else{
+            print("handle error here..")
+            return
+        
+        }
+        
+        let contact = Data(fullname: textname)
+        print(contact.fullname)
+        delegate?.addData(saveDatas: contact)
+        self.dismiss(animated: true, completion: nil)
+        
     }
-    */
-
+    
+    @objc func handleCancel() {
+        self.dismiss(animated: true, completion: nil)
+        
+    }
 }
+
